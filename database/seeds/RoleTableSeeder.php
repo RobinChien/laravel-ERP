1b5689dfe863b138139ba5280ebab232081d317b
goodnana1224 <goodnana1224@gmail.com>
2018-01-08 02:53:17 +0800

    Update RoleTableSeeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -15,10 +15,21 @@ class RoleTableSeeder extends Seeder
     {
 
         $permission_s01 = Permission::where('name', 's01')->first();
+        $permission_user_index = Permission::where('name', 'user-index')->first();
+        $permission_user_show = Permission::where('name', 'user-show')->first();
+        $permission_user_create = Permission::where('name', 'user-create')->first();
+        $permission_user_edit = Permission::where('name', 'user-edit')->first();
+        $permission_user_status = Permission::where('name', 'user-status')->first();
+        $permission_user_delete = Permission::where('name', 'user-delete')->first();
         $permission_role_index = Permission::where('name', 'role-index')->first();
+        $permission_role_show = Permission::where('name', 'role-show')->first();
         $permission_role_create = Permission::where('name', 'role-create')->first();
         $permission_role_edit = Permission::where('name', 'role-edit')->first();
         $permission_role_delete = Permission::where('name', 'role-delete')->first();
+        $permission_permission_index = Permission::where('name', 'permission-index')->first();
+        $permission_permission_show = Permission::where('name', 'permission-show')->first();
+        $permission_permission_edit = Permission::where('name', 'permission-edit')->first();
+        $permission_permission_status = Permission::where('name', 'permission-status')->first();
 
         $admin = new Role();
         $admin->name = 'admin';
@@ -26,7 +37,18 @@ class RoleTableSeeder extends Seeder
         $admin->description = 'User has access to all system functionality';
         $admin->save();
         $admin->permissions()->attach($permission_s01);
+        $admin->permissions()->attach($permission_user_index);
+        $admin->permissions()->attach($permission_user_show);
+        $admin->permissions()->attach($permission_user_create);
+        $admin->permissions()->attach($permission_user_edit);
+        $admin->permissions()->attach($permission_user_status);
+        $admin->permissions()->attach($permission_user_delete);
+        $admin->permissions()->attach($permission_permission_index);
+        $admin->permissions()->attach($permission_permission_show);
+        $admin->permissions()->attach($permission_permission_edit);
+        $admin->permissions()->attach($permission_permission_status);
         $admin->permissions()->attach($permission_role_index);
+        $admin->permissions()->attach($permission_role_show);
         $admin->permissions()->attach($permission_role_create);
         $admin->permissions()->attach($permission_role_edit);
         $admin->permissions()->attach($permission_role_delete);
